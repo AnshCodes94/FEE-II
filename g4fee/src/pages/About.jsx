88e086d0f4315ef1d292dfb3f675d7aa10f1bb95
Ansh Bansal <ansh122021@gmail.com>
2026-09-13 18:46:14 +0530

    updated files

--- g4fee/src/pages/About.jsx
+++ g4fee/src/pages/About.jsx
@@ -2,7 +2,7 @@ export function About() {
     return (
         <div className="fy p2 bg2" style={{ height: "calc(100dvh - 5rem)" }}>
             <h3>About us</h3>
-            <h1>ABOUT SECTION</h1>
+            <h1>HI I am Ansh. A Computer science student at Chitkara university</h1>
         </div>
     );
 }
